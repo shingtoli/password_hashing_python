6131e3408621f17123b8c55a6e1f82956d253fb4
shingtoli <shingto.li@sestorobotics.com>
2021-02-01 13:20:40 +0800

    Working password shell

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from cmd import Cmd
 from os import urandom
+from getpass import getpass
 from hashlib import pbkdf2_hmac
 from pymongo import MongoClient
 
@@ -18,16 +20,50 @@ def hash_password(password_input, salt):
     return new_key
 
 
-def main():
+class PasswordShell(Cmd):
+    intro = 'Welcome to Password Hashing Test Shell.\nType help or ? to list commands.\n'
+    prompt = '(hash) '
+    file = None
     db = get_database()
-    salt = urandom(32)
-    key = hash_password('mypassword', salt)
-    some_user = {
-        "username": "some_user",
-        "password": salt + key,
-    }
-    db["users"].insert_one(some_user)
 
+    def do_create(self, arg):
+        username = input("Username: ")
+
+        found_user = self.db["users"].find_one({'username': username})
+        if found_user:
+            print('Username already taken.')
+            return
+
+        password = getpass("Password: ")
+        print(arg)
+
+        salt = urandom(32)
+        key = hash_password(password, salt)
+
+        some_user = {
+            "username": username,
+            "password": salt + key,
+        }
+        self.db["users"].insert_one(some_user)
+
+        print(f"User {username} is created")
+
+    def do_login(self, arg):
+        username = input("Username: ")
+        found_user = self.db["users"].find_one({'username': username})
+        if not found_user:
+            print('User not found')
+            return
+        salt = found_user["password"][:32]
+        key = found_user["password"][32:]
+        
+        password_input = getpass("Password: ")
+        check_key = hash_password(password_input, salt)
+        
+        if key == check_key:
+            print(f'Login Successful! Welcome {username}.')
+        else:
+            print('Login failed.')
 
 if __name__ == "__main__":
-    main()
+    PasswordShell().cmdloop()
